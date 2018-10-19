65d9b604af8a849749d232f72d5b827fafa8380f
JeffreyRJohnson <jeffreyrjohnsonsemail@gmail.com>
2018-10-19 09:25:25 -0500

    add expense summary to dashboard

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -9,8 +9,8 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
       <h3>{description}</h3>
     </Link>
     <p>
-      {numeral(amount / 100).format('0,0.00')}-
-      {moment(createdAt).format('MMMM do YYYY')}
+      {numeral(amount / 100).format('$0,0.00')} -
+      {moment(createdAt).format(' MMMM do YYYY')}
     </p>
   </div>
 );
